74ffa4bebd6880e7a7dc430c901604a104f249d7
JACK-QBS <2200838299@qq.com>
2021-05-24 10:37:37 +0800

    统一数据封装

---
+++ 博客项目（框架版）/org.example/config/ResponseAdvice.java
@@ -0,0 +1,56 @@
+package org.example.config;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.example.model.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * 统一数据封装
+ */
+//@ControllerAdvice
+@Slf4j
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        //所有请求映射方法都给冲洗掉
+        return true;
+    }
+
+    /**
+     * 这个方法无法解决请求映射方法返回类型为Object，但是返回值为null，需要包裹统一数据格式的问题
+     * 原因是SpringMVC框架在这种逻辑条件下都不会进入这个方法
+     */
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        //如果 当前类型 属于 response对象，直接等于 body,直接返回 body
+        if (body instanceof Response) {
+            return body;
+        }
+        //如果是非自定义类型，在外面包一层resp
+        Response resp = new Response();
+        resp.setOk(true);
+        resp.setData(body);
+        //如果返回类型为Object，返回值为String，或是返回类型是String，则需要包裹后再序列化为相同的String类型
+        if (body instanceof String || returnType.getParameterType().equals(String.class)) {
+            try {
+                response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                return objectMapper.writeValueAsString(resp);
+            } catch (JsonProcessingException e) {
+                log.error("序列化响应失败",e);
+            }
+        }
+        return resp;
+    }
+}
