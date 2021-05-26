9748702f3b6442b8556ed8c781e62aec8aa49e59
JACK-QBS <2200838299@qq.com>
2021-05-26 16:06:30 +0800

    支付宝付款

---
+++ AOP/静态代理/AliPayService.java
@@ -0,0 +1,11 @@
+package org.example.proxy.proxy;
+
+//支付宝付款
+public class AliPayService implements PayService {
+    
+    @Override
+    public void pay() {
+        //支付业务逻辑
+        System.out.println("支付宝支付...");
+    }
+}
