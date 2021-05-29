a1ef2367b82e82d7030db2df6362ea3844f49424
JACK-QBS <2200838299@qq.com>
2021-05-29 14:31:21 +0800

    文章接口

---
+++ mybatis/java/org/example/mapper/ArticleMapper.java
@@ -0,0 +1,16 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.model.Article;
+import org.example.model.Article;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+public interface ArticleMapper {
+
+    //1对1映射，一个文章关联1个用户
+    List<Article> selectAll();
+}
