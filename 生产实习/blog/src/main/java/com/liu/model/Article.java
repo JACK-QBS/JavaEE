4e318c5fe624fa2fc81b3746ef495a64a3da98ab
JACK-QBS <2200838299@qq.com>
2021-07-16 15:06:31 +0800

    Article

---
+++ 生产实习/blog/src/main/java/com/liu/model/Article.java
@@ -0,0 +1,32 @@
+package com.liu.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class Article {
+    private Integer id;
+    private Integer categoryId;
+    private String title;
+    private String content;
+    private String description;
+    private Integer statue;
+    private String author;
+    private Date createTime;
+    private Integer showCount;
+    //新增属性，处理博客的多标签
+    private List<ArticleTag> tagList;
+    //新增属性，处理事件展示
+    private String time;
+    //新增属性，展示博客类别
+    private String categoryName;
+    //新增属性，获取选择标签id
+    private Integer tagId;
+}
