6f4e2527b33271e3a9761b6c7c5062448ed01c08
JACK-QBS <2200838299@qq.com>
2021-07-17 15:41:06 +0800

    Category

---
+++ 生产实习/blog/src/main/java/com/liu/model/Category.java
@@ -0,0 +1,14 @@
+package com.liu.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Category {
+    private Integer id;
+    private String categoryName;
+}
